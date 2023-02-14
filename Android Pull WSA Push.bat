68b57fac74b04e2c125b3b03de6cc51983e510df
Shaptak Naskar <57929079+ShaptakNaskar@users.noreply.github.com>
2023-02-14 12:27:44 +0530

    Add some QOL Changes

--- Android Pull WSA Push.bat
+++ Android Pull WSA Push.bat
@@ -1,13 +1,22 @@
 @echo off
+:CHOICE2
+cls
 adb disconnect
-ECHO.
+cls
+ECHO WELCOME TO ADB-CMD-WSA Pull-Push Utility
+ECHO Please choose desired PULL Method from Phone
 ECHO 1. USB PULL
 ECHO 2. Network Pull
+ECHO 3. Just WSA Push
+ECHO 4. EXIT
 set choice=
 set /p choice=Choose any one: 
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='1' goto USB
 if '%choice%'=='2' goto NET
+if '%choice%'=='3' goto WSA
+if '%choice%'=='4' goto EXIT
+if '%choice%'=='5' goto exitchoice
 ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
@@ -45,3 +54,15 @@ adb push . /sdcard/Pushed/
 adb push %userprofile%\Videos\. /sdcard/Pushed/UserVids/
 echo Done
 pause
+cls
+:exitchoice
+ECHO.
+ECHO 1. EXIT
+ECHO 2. Go to Main Menu
+set choice4=
+set /p choice4=Choose any one: 
+if not '%choice4%'=='' set choice4=%choice4:~0,1%
+if '%choice4%'=='1' goto EXIT
+if '%choice4%'=='2' goto CHOICE2
+:EXIT
+exit
